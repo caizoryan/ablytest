acee13e97c12d9ea4e0419c4622c83b2e8d3c628
Aaryan Pashine <pashineaaryan@gmail.com>
2025-12-04 11:59:26 -0500

    ok

--- remote.js
+++ remote.js
@@ -3,14 +3,6 @@ import { reactive, memo } from './hok.js'
 import { dom } from './dom.js'
 
 let send = () => console.log('not conntexted')
-let input = dom("input", {
-	onkeydown: e => e.key == 'Enter' ? send(e.target.value) : null
-})
-let btn = dom('button', { onclick: () => send(input.value) }, 'set')
-
-document.body.appendChild(input)
-document.body.appendChild(btn)
-
 async function publishSubscribe() {
 	const ably = new dawg.Ably.Realtime("f8JcZg.XRPrxA:MdTyXnycB5ahByz--1CIY0-BMFLvLBIz8cSMgS6vTNM")
 	ably.connection.once("connected", () => { console.log("Connected to Ably!") })
